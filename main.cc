e08b1d1152ff3fc895433e70ba87574972fcf1c0
ototsuyume <ototsuyume@gmail.com>
2016-04-18 23:32:50 +0200

    Add new code.

--- main.cc
+++ main.cc
@@ -1,5 +1,12 @@
 #include "engine.h"
 
 int main(){
+  unordered_map<string, FakeKATON::RecordType> fields;
+  fields["address"] = FakeKATON::RecordType::rtString;
+  fields["phone"] = FakeKATON::RecordType::rtInt;
+  fields["data"] = FakeKATON::RecordType::rtFloat;
+  auto db = FakeKATON::Engine::NewEngine(fields);
+  if (db->WriteString("Andrew", "address", "101 No.1 Street"))
+    fprintf(stderr, "Write string failed.\n");
   return 0;
 }
